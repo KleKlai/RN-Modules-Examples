94f18c7fe77ce562ab8474fca4cdd6357acb1575
KleKlai <magallenmaynards@gmail.com>
2023-11-20 13:52:52 +0800

    feat: basic implementation of mapview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,18 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import MapView from 'react-native-maps';
 
 export default function TabOneScreen() {
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Example of Map View</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View>
+        <MapView 
+        style={{ width: '100%', height: '100%'}} />
+      </View>
     </View>
   );
 }
@@ -16,8 +21,6 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
