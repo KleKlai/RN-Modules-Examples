f35c0b6c9cde8cfd4d77e4ca334b1d3d86621b76
KleKlai <magallenmaynards@gmail.com>
2023-11-20 12:15:55 +0800

    feat: added basic location permission

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,40 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function TabOneScreen() {
+
+  const [location, setLocation] = useState<any>();
+  const [errorMsg, setErrorMsg] = useState<string>('');
+
+  useEffect(() => {
+    (async () => {
+      
+      var { status } = await Location.requestForegroundPermissionsAsync();
+      if (status != 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      var location = await Location.getCurrentPositionAsync({});
+      setLocation(location)
+    })();
+  }, [])
+
+  var text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text>{text}</Text>
     </View>
   );
 }
