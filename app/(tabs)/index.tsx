1bc9b7cb7e6b1cc12972d1d6668b5ee9053911a6
KleKlai <magallenmaynards@gmail.com>
2023-11-20 14:12:44 +0800

    feat: add dynamic location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,41 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function TabOneScreen() {
+  const [location, setLocation] = useState<any>();
+  const [errorMsg, setErrorMsg] = useState<string>();
+
+  // You can make it dynamic by using Expo Location
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 7.151153,  
+    longitude: 125.599511,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  }
+
+  useEffect(() => {
+    userLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -12,7 +44,13 @@ export default function TabOneScreen() {
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View>
         <MapView 
-        style={{ width: '100%', height: '100%'}} />
+        region={mapRegion}
+        zoomEnabled
+        zoomTapEnabled
+        minZoomLevel={18}
+        style={{ width: '100%', height: '100%'}}>
+          <Marker coordinate={mapRegion} title='Marker'/>
+        </MapView>
       </View>
     </View>
   );
