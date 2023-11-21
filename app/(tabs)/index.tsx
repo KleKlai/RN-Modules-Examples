2b2e0ab755bfc4d0fba6d5df089de1e2d062d0eb
KleKlai <magallenmaynards@gmail.com>
2023-11-21 18:21:05 +0800

    feat: added bottomsheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
+import React, { useRef } from 'react';
+
+import BottomSheet, { BottomSheetMethods } from '@devvie/bottom-sheet';
 export default function TabOneScreen() {
+  const sheetRef = useRef<BottomSheetMethods>(null);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Open" onPress={() => sheetRef.current.open()} />
+      <BottomSheet 
+      height={'75%'} 
+      ref={sheetRef}>
+        <Text>
+          The smart 😎, tiny 📦, and flexible 🎗 bottom sheet your app craves 🚀
+        </Text>
+      </BottomSheet>
     </View>
   );
 }
